97d61946e5a681d931c5e56fb2f93ffd33cceca9
Navid <n.kazempour@gmail.com>
2016-04-26 18:25:56 +0000

    routing root to movies#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
-Rails.application.routes.draw do
-  
-  get 'reviews/new'
+RottenMangoes::Application.routes.draw do
 
-  get 'reviews/create'
-
-resources :movies do
-  resources :reviews, only: [:new, :create]
-end
-resources :users, only: [:new, :create]
-resources :sessions, only: [:new, :create, :destroy]
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
 
 end
