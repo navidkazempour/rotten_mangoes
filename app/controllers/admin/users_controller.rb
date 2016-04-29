ff94e8e7f96a12674acf05ba960c9c0152c1b9bb
Navid <n.kazempour@gmail.com>
2016-04-29 00:48:27 +0000

    cleaned up

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -42,7 +42,7 @@ class Admin::UsersController < ApplicationController
 
     def destroy
       @user = User.find(params[:id])
-      UserMailer.welcome_email(@user).deliver
+      UserMailer.delete_email(@user).deliver
       @user.destroy
       redirect_to admin_users_path
     end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,7 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@example.com'
  
-  def welcome_email(user)
-    @user = user
-    mail(to: @user.email,
-         subject: 'Welcome to My Awesome Site')
+  def delete_email(user)
+   
   end
 end
