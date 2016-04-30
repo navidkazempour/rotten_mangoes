8a3996944a7814d5fbd53884d715b731b81ddf68
Navid <n.kazempour@gmail.com>
2016-04-30 14:49:32 +0000

    db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-20.times do 
-  User.create!(firstname: Faker::Name.first_name, 
-    lastname: Faker::Name.last_name, 
-    email: Faker::Internet.email, 
-    password: "mangouser", 
-    password_confirmation: "mangouser")
+
+# 20.times do 
+#   User.create!(firstname: Faker::Name.first_name, 
+#     lastname: Faker::Name.last_name, 
+#     email: Faker::Internet.email, 
+#     password: "mangouser", 
+#     password_confirmation: "mangouser")
+# end
+
+
+@all_movies = Imdb::Top250.new.movies
+@all_movies.each do |num|
+  num = num.id
+  movie = Imdb::Movie.new(num)
+  Movie.create!(title: movie.title.gsub(/\d*\.\s*/, ''),
+  release_date: movie.release_date,
+  director: movie.director,
+  runtime_in_minutes: movie.length,
+  poster_image_url: movie.poster,
+  description: movie.plot)
 end
